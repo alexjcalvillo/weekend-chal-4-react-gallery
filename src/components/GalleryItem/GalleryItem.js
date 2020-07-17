1f2450472b06d036b1e27f4b1c12b0b0b4dde5ca
Alexander Calvillo <alexanderjcalvillo@gmail.com>
2020-07-17 16:58:50 -0500

    working on gallery item

---
+++ src/components/GalleryItem/GalleryItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import styles from '../GalleryItem/GalleryItem.module.css';
+
+class GalleryList extends Component {
+  render() {
+    const { item } = this.props;
+    return <div>Left off here!</div>;
+  }
+}
+
+export default GalleryItem;
--- src/components/GalleryList/GalleryList.js
+++ src/components/GalleryList/GalleryList.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import styles from '../GalleryList/GalleryList.module.css';
+import GalleryItem from '../GalleryItem/GalleryItem';
 
 class GalleryList extends Component {
   render() {
-    const displayedGallery = this.props.group.map((image, index) => {
-      return (
-        <div className={styles.item} key={index}>
-          <img src={image.path} alt={image.description}></img>
-          <h4>{image.likes}</h4>
-          <button>Like!</button>
-        </div>
-      );
-    });
-    return <div>{displayedGallery}</div>;
+    // const displayedGallery = this.props.group.map((image, index) => {
+    //   return (
+    //     <div className={styles.item} key={index}>
+    //       <img src={image.path} alt={image.description}></img>
+    //       <h4>{image.likes}</h4>
+    //       <button>Like!</button>
+    //     </div>
+    //   );
+    // });
+    return (
+      <div>
+        <GalleryItem image={this.props.items} />
+      </div>
+    );
   }
 }
 
